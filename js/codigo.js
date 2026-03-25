10a0dfe2295aa71eb27cfec77270ab9a7b6a430f
lionsyseng <lionsyseng@gmail.com>
2026-03-25 12:57:08 +0100

    Septimo commit

--- js/codigo.js
+++ js/codigo.js
@@ -1,3 +1,24 @@
-if ('ontouchstart' in window) {
-  document.body.classList.add('touch');
-}
+/** Función para direccionar a GitHub **/
+const github = document.getElementById("github").addEventListener("click", function() {
+    window.open("https://github.com/", "_blank");
+});
+
+/** Función para direccionar a Frontend Mentor **/
+const fontendMentor = document.getElementById("frontend").addEventListener("click", function() {
+    window.open("https://www.frontendmentor.io/", "_blank");
+});
+
+/** Función para direccionar a Linkedin **/
+const linkedin = document.getElementById("linkedin").addEventListener("click", function() {
+    window.open("https://l1nk.dev/linkedin-login", "_blank");
+});
+
+/** Función para direccionar a Twitwe **/
+const twiter = document.getElementById("twiter").addEventListener("click", function() {
+    window.open("https://x.com/", "_blank");
+});
+
+/** Función para direccionar a Instagram **/
+const instagram = document.getElementById("button").addEventListener("click", function() {
+    window.open("https://www.instagram.com/", "_blank");
+});
